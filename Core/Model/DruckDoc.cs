75bdae1ca6735afaf09f71b5a9557dfd08c7b8ca
jelemux <yeremiah14@gmail.com>
2019-09-10 13:24:43 +0200

    druckfunktion

---
+++ Core/Model/DruckDoc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class DruckDoc
+    {
+        public string AuftragNr { get; set; }
+        public string KundeName { get; set; }
+        public string KundeAnschrift { get; set; }
+        public string KundeTelefon { get; set; }
+        public List<Taetigkeit> TatListe { get; set; }
+        public List<Mitarbeiter> MitList { get; set; }
+
+        public DruckDoc()
+        {
+
+        }
+    }
+}
