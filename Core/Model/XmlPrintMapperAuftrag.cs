3e1e5a5b658fac45a409f49649eb2b69bbae5565
jelemux <yeremiah14@gmail.com>
2019-09-13 10:10:02 +0200

    Pmapper1

---
+++ Core/Model/XmlPrintMapperAuftrag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class XmlPrintMapperAuftrag
+    {
+        //Test
+        public List<PrintMapperItem> MappingList { get; set; }
+        public int Start { get; set; }
+        public int Inc { get; set; }
+        public List<PrintMapperTaetigkeit> TatList { get; set; }
+    }
+}
